f58563d57fb7327be3f1fa21d3964fd5837df7de
janphiliprichter <janphiliprichter@gmail.com>
2023-07-24 16:04:30 +0200

    Renaming file

--- code/Non-parametric Regression.R
+++ code/Non-parametric Regression.R
@@ -7,15 +7,15 @@ set.seed(42)
 ### GAM
 
 mod_gam <- gam(log_count ~ s(temperature) + s(dp_temperature) + 
-      s(humidity) + s(log_wind_speed) + s(sin_hour) + s(cos_hour) + 
+      s(humidity) + s(log_wind_speed) + sin_hour + cos_hour + 
         s(log_rainfall) + s(solar_radiation) + s(sqrt_visibility) + 
         sin_dow + cos_dow + spring + summer + winter, 
       data = train)
+
+summary(mod_gam)
 
 plot(mod_gam)
 
 BIC(mod_gam)
 
-pred_gam <- predict(mod_gam, newdata = test)
-MSE(pred_gam, test$log_count)
 

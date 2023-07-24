b8ca1cb3f02e6b68e3e1f7c55faecdd316ddc66e
janphiliprichter <janphiliprichter@gmail.com>
2023-07-24 16:05:10 +0200

    Added comments

--- code/Non-parametric Regression.R
+++ code/Non-parametric Regression.R
@@ -1,21 +1,31 @@
+# Loading libraries
 library(npreg)
 library(mgcv)
 library(gplm)
+
+# Setting random seed
 set.seed(42)
 
 
+##### Non-parametric Regression #####
+
 ### GAM
 
+# Fitting the model
 mod_gam <- gam(log_count ~ s(temperature) + s(dp_temperature) + 
       s(humidity) + s(log_wind_speed) + sin_hour + cos_hour + 
         s(log_rainfall) + s(solar_radiation) + s(sqrt_visibility) + 
         sin_dow + cos_dow + spring + summer + winter, 
       data = train)
 
+# Summary output
 summary(mod_gam)
 
+# Plot of the smoothing effects
 plot(mod_gam)
 
+# BIC
 BIC(mod_gam)
+
 
 
